8d02f5c1bd0cd64411dedae9f90b50d0bcf06897
zhoutao1231 <760469291@qq.com>
2020-01-23 23:47:11 +0800

    Site updated: 2020-01-23 23:47:10

---
+++ js/src/custom.js
@@ -0,0 +1,10 @@
+/* 轮播背景图片 */
+$(function () {
+    $.backstretch([
+        "/images/background/bg1.jpg",
+        "/images/background/bg2.jpg",
+        "/images/background/bg3.jpg",
+	"/images/background/bg4.jpg",
+	"/images/background/bg5.jpg",
+    ], {duration: 4000, fade: 1500});
+});
